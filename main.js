a72507389b74faf02ba1336256b61478b3a19956
yamaguchi371 <telework-takefu6@outlook.jp>
2021-06-30 14:52:36 +0900

    完成

--- main.js
+++ main.js
@@ -15,10 +15,18 @@ var todoStorage = {
     }
 }
 
-const app = new Vue ({
+var app = new Vue ({
     el: '#app',
     data: {
-        todos: []
+        todos: [],
+        options: [
+            { value: -1, label: 'すべて' },
+            { value: 0, label: '作業中' },
+            { value: 1, label: '完了' }
+        ],
+        // 選択しているoptionのvalueを記憶するためのデータ
+        // 初期値を「ー１」つまり「すべて」にする
+        current: -1
     },
     methods: {
         
@@ -40,6 +48,44 @@ const app = new Vue ({
             })
             // フォーム要素を空にする
             comment.value = ''
+        },
+        // 状態変更の処理
+        doChangeState: function(item) {
+            item.state = item.state ? 0 : 1
+        },
+        // 削除の処理
+        doRemove: function(item) {
+            var index = this.todos.indexOf(item)
+            this.todos.splice(index, 1)
+        }
+    },
+    watch: {
+        // オプションを使う場合はオブジェクト形式にする
+        todos: {
+            // 引数はウォッチしているプロパティの変更後の値
+            handler: function(todos) {
+                todoStorage.save(todos)
+            },
+            // deepオプションでネストしているデータも監視できる
+            deep: true
+        }
+    },
+    created() {
+        // インスタンス作成時に自動的にfetch()する
+        this.todos = todoStorage.fetch()
+    },
+    computed: {
+        labels() {
+            return this.options.reduce(function(a, b) {
+                return Object.assign(a, { [b.value]: b.label })
+            }, {})
+        },
+        computedTodos: function() {
+            // データがcurrentがー１ならすべて
+            // それ以外ならcurrentとstateが一致するものだけに絞り込む
+            return this.todos.filter(function(el) {
+                return this.current < 0 ? true : this.current === el.state
+            }, this)
         }
     }
 })
